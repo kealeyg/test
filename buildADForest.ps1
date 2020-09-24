41fb02fbb4a34a78612713fe536ddcbc5b496540
Greg <Gregory.Kealey@canada.ca>
2020-09-24 12:23:26 -0400

    Update buildADForest.ps1

--- buildADForest.ps1
+++ buildADForest.ps1
@@ -1,3 +1,4 @@
 Install-WindowsFeature -IncludeAllSubFeature RSAT
 Add-WindowsFeature AD-Domain-Services
 Install-ADDSForest -DomainName "gktest.local" -DomainMode Win2012R2 -ForestMode Win2012R2 -DatabasePath "c:\NTDS" -SysvolPath "c:\SYSVOL" -LogPath "c:\Logs" -DomainNetbiosName "gktest" -InstallDns -NoRebootOnCompletion -Force
+exit 0
