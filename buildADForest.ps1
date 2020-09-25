566b9bd31ea4b300a8e778b7461c76c237664214
Greg <Gregory.Kealey@canada.ca>
2020-09-25 19:15:47 -0400

    Update buildADForest.ps1

--- buildADForest.ps1
+++ buildADForest.ps1
@@ -1,33 +1,34 @@
-workflow buildAD {
-    $domain = "gktest"
-    $SecurePassword = ConvertTo-SecureString 'Canada1!' -asplaintext -force 
-    $TextInfo = (Get-Culture).TextInfo
+$domain = "gktest"
+$password = 'Canada1!'
+$SecurePassword = ConvertTo-SecureString $password -asplaintext -force 
+$TextInfo = (Get-Culture).TextInfo
 
-    Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
-    Install-ADDSForest -DomainName "${domain}.local" -DomainMode Win2012R2 -ForestMode Win2012R2 -DatabasePath "c:\NTDS" -SysvolPath "c:\SYSVOL" -LogPath "c:\Logs" -DomainNetbiosName "${domain}" -InstallDns -NoRebootOnCompletion -Force -SafeModeAdministratorPassword $SecurePassword
+Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+Install-ADDSForest -DomainName "${domain}.local" -DomainMode Win2012R2 -ForestMode Win2012R2 -DatabasePath "c:\NTDS" -SysvolPath "c:\SYSVOL" -LogPath "c:\Logs" -DomainNetbiosName "${domain}" -InstallDns -NoRebootOnCompletion -Force -SafeModeAdministratorPassword $SecurePassword
 
-    Restart-Computer -Wait
+$createUsersScript = @"
 
-    $users = ConvertFrom-Json -InputObject '[
-        {"firstName":"tarek","lastName":"ali"},
-        {"firstName":"gerald","lastName":"hill"},
-        {"firstName":"simon","lastName":"earl"},
-        {"firstName":"adam","lastName":"huisman"},
-	    {"firstName":"greg","lastName":"kealey"},
-        {"firstName":"tho","lastName":"nguyen"}
-    ]';
+`$TextInfo = (Get-Culture).TextInfo
+`$SecurePassword = ConvertTo-SecureString "$password" -asplaintext -force 
 
-    foreach($user in $users){
-	    $firstName = $user.firstName
-	    $lastName = $user.lastName
-	    $name = "${firstName} ${lastName}"
-	    $name = $TextInfo.ToTitleCase($name.ToLower())
-	    $samName = $firstName[0] + "${lastName}"
-	    $UPName = "${samName}@${domain}.local"
-	    $path = "CN=Users,DC=${domain},DC=local"
-	    New-ADUSER -Name $name -GivenName $TextInfo.ToTitleCase($firstName) -Surname $TextInfo.ToTitleCase($lastName) -SamAccountName $samName -userprincipalname $UPName -Path $path -AccountPassword $SecurePassword -Enable $true
+`$users = ConvertFrom-Json -InputObject '[
+	{"firstName":"greg","lastName":"kealey"}
+]';
+
+foreach(`$user in `$users){
+	`$firstName = `$user.firstName
+	`$lastName = `$user.lastName
+	`$name = "`${firstName} `${lastName}"
+	`$name = `$TextInfo.ToTitleCase(`$name.ToLower())
+	`$samName = "`${lastName}" + `$firstName[0]
+	`$UPName = "`${samName}@${domain}.local"
+	`$path = "CN=Users,DC=${domain},DC=local"
+    if (-not [bool] (Get-ADUser -Filter { SamAccountName -eq `$samName })) {
+	    New-ADUSER -Name `$name -GivenName `$TextInfo.ToTitleCase(`$firstName) -Surname `$TextInfo.ToTitleCase(`$lastName) -SamAccountName `$samName -userprincipalname `$UPName -Path `$path -AccountPassword `$SecurePassword -Enable `$true
     }
-    exit 0
 }
+"@
 
-buildAD
+New-Item -Path C:\ -Name "createUsers.ps1" -ItemType "file" -Value $createUsersScript
+Restart-Computer
+exit 0
