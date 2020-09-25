872f47a5b36c62e4fc660ef0d1683e38ba93b6eb
Greg <Gregory.Kealey@canada.ca>
2020-09-25 12:18:06 -0400

    securepassword
    test

--- buildADForest.ps1
+++ buildADForest.ps1
@@ -1,3 +1,4 @@
 Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
-Install-ADDSForest -DomainName "gktest.local" -DomainMode Win2012R2 -ForestMode Win2012R2 -DatabasePath "c:\NTDS" -SysvolPath "c:\SYSVOL" -LogPath "c:\Logs" -DomainNetbiosName "gktest" -InstallDns -NoRebootOnCompletion -Force -SafeModeAdministratorPassword "Canada1!" 
+$SecurePassword = ConvertTo-SecureString 'Canada1!' –asplaintext –force
+Install-ADDSForest -DomainName "gktest.local" -DomainMode Win2012R2 -ForestMode Win2012R2 -DatabasePath "c:\NTDS" -SysvolPath "c:\SYSVOL" -LogPath "c:\Logs" -DomainNetbiosName "gktest" -InstallDns -NoRebootOnCompletion -Force -SafeModeAdministratorPassword $SecurePassword
 exit 0
