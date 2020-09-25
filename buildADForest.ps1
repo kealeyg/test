947814b0bf8350a78af10fc93c2d8c50ba9dbb0c
Greg <Gregory.Kealey@canada.ca>
2020-09-25 16:30:59 -0400

    Update buildADForest.ps1

--- buildADForest.ps1
+++ buildADForest.ps1
@@ -1,29 +1,33 @@
-$domain = "gktest"
-$SecurePassword = ConvertTo-SecureString 'Canada1!' -asplaintext -force 
-$TextInfo = (Get-Culture).TextInfo
+workflow buildAD {
+    $domain = "gktest"
+    $SecurePassword = ConvertTo-SecureString 'Canada1!' -asplaintext -force 
+    $TextInfo = (Get-Culture).TextInfo
 
-Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+    Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+    Install-ADDSForest -DomainName "${domain}.local" -DomainMode Win2012R2 -ForestMode Win2012R2 -DatabasePath "c:\NTDS" -SysvolPath "c:\SYSVOL" -LogPath "c:\Logs" -DomainNetbiosName "${domain}" -InstallDns -NoRebootOnCompletion -Force -SafeModeAdministratorPassword $SecurePassword
 
-Install-ADDSForest -DomainName "${domain}.local" -DomainMode Win2012R2 -ForestMode Win2012R2 -DatabasePath "c:\NTDS" -SysvolPath "c:\SYSVOL" -LogPath "c:\Logs" -DomainNetbiosName "${domain}" -InstallDns -NoRebootOnCompletion -Force -SafeModeAdministratorPassword $SecurePassword
+    Restart-Computer -Wait
 
-$users = ConvertFrom-Json -InputObject '[
-    {"firstName":"tarek","lastName":"ali"},
-    {"firstName":"gerald","lastName":"hill"},
-    {"firstName":"simon","lastName":"earl"},
-    {"firstName":"adam","lastName":"huisman"},
-	{"firstName":"greg","lastName":"kealey"},
-    {"firstName":"tho","lastName":"nguyen"}
-]';
+    $users = ConvertFrom-Json -InputObject '[
+        {"firstName":"tarek","lastName":"ali"},
+        {"firstName":"gerald","lastName":"hill"},
+        {"firstName":"simon","lastName":"earl"},
+        {"firstName":"adam","lastName":"huisman"},
+	    {"firstName":"greg","lastName":"kealey"},
+        {"firstName":"tho","lastName":"nguyen"}
+    ]';
 
-foreach($user in $users){
-	$firstName = $user.firstName
-	$lastName = $user.lastName
-	$name = "${firstName} ${lastName}"
-	$name = $TextInfo.ToTitleCase($name.ToLower())
-	$samName = $firstName[0] + "${lastName}"
-	$UPName = "${samName}@${domain}.local"
-	$path = "CN=Users,DC=${domain},DC=local"
-	New-ADUSER -Name $name -GivenName $TextInfo.ToTitleCase($firstName) -Surname $TextInfo.ToTitleCase($lastName) -SamAccountName $samName -userprincipalname $UPName -Path $path -AccountPassword $SecurePassword -Enable $true
+    foreach($user in $users){
+	    $firstName = $user.firstName
+	    $lastName = $user.lastName
+	    $name = "${firstName} ${lastName}"
+	    $name = $TextInfo.ToTitleCase($name.ToLower())
+	    $samName = $firstName[0] + "${lastName}"
+	    $UPName = "${samName}@${domain}.local"
+	    $path = "CN=Users,DC=${domain},DC=local"
+	    New-ADUSER -Name $name -GivenName $TextInfo.ToTitleCase($firstName) -Surname $TextInfo.ToTitleCase($lastName) -SamAccountName $samName -userprincipalname $UPName -Path $path -AccountPassword $SecurePassword -Enable $true
+    }
+    exit 0
 }
 
-exit 0
+buildAD
