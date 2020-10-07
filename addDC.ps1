82ee1d98164c17f40e76c06f9c34e6bcfce4d53d
Greg <Gregory.Kealey@canada.ca>
2020-10-07 11:24:29 -0400

    Update addDC.ps1

--- addDC.ps1
+++ addDC.ps1
@@ -1,14 +1,20 @@
 ﻿$domain = $args[0]
 $password = $args[1]
-$SecurePassword = ConvertTo-SecureString $password -asplaintext -force 
+$user = $args[2]
+
 $TextInfo = (Get-Culture).TextInfo
 
 $script = @"
+`$user = "${domain}.local\${user}"
+`$SecurePassword = ConvertTo-SecureString "$password" -asplaintext -force 
+`$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, `$SecurePassword
+Add-Computer -DomainName ${domain}.local -Credential `$credential -Force
+
 Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
-Install-ADDSDomainController -InstallDns -DomainName `$domain -NoRebootOnCompletion -Force -SafeModeAdministratorPassword `$SecurePassword
+Install-ADDSDomainController -InstallDns -DomainName ${domain}.local -NoRebootOnCompletion -Force -SafeModeAdministratorPassword `$SecurePassword
 Add-DnsServerForwarder -IPAddress 168.63.129.16 -PassThru
 "@
 
 New-Item -Path C:\ -Name "buildDC.ps1" -ItemType "file" -Value $script
-Restart-Computer -Force
-exit 0
+#Restart-Computer -Force
+#exit 0
